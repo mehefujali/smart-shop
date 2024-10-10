34eb28084d4891c70a2455dfb9f9cb8fc3e5ba13
Mehefuj Ali <mehefujalim@gmail.com>
2024-10-11 00:30:03 +0530

    all product fetch complited

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 const lodeAllProduct = async () => {
       const res = await fetch('https://dummyjson.com/products')
       const data = await res.json()
-      console.log(data.products);
+      displayProducts(data.products)
 
 }
 
 lodeAllProduct()
+
+const displayProducts = (data) => {
+      const productContainer = document.getElementById("productsContainer")
+      data.map(product => {
+            const { title, thumbnail } = product
+            const productCard = document.createElement("div")
+            productCard.classList = " flex flex-col gap-3 border rounded-lg border-blue-300 p-6"
+            productCard.innerHTML = `
+            <img src="${thumbnail}">
+                   <h1 class=" text-2xl">${title}</h1>
+            `
+
+            productContainer.appendChild(productCard)
+      })
+}
