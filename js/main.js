83da1b01ad90318ca89b15b6460dbdb833af2ed7
Mehefuj Ali <mehefujalim@gmail.com>
2024-10-11 11:21:06 +0530

    conditional css added in reting

--- js/main.js
+++ js/main.js
@@ -22,12 +22,13 @@ const displayProducts = (data) => {
             const { title, thumbnail, price, discountPercentage, rating } = product
             const newPrice = price - (discountPercentage / 100) * price
             const productCard = document.createElement("div")
-            productCard.classList = " flex flex-col gap-3 border rounded-lg border-blue-300 p-6 product-card hover:shadow-md"
+            productCard.classList = " flex flex-col gap-3 border rounded-lg border-blue-300 p-6 product-card hover:shadow-md justify-between"
             productCard.innerHTML = `
             <img src="${thumbnail}">
                    <h1 class=" text-xl md:text-2xl font-bold">${title}</h1>
                    <p class=" "><span class="line-through">$${price}</span>    <span class="text-green-400 font-bold">${discountPercentage}%</span> </p>
                    <p class=" font-bold">$${newPrice.toFixed(2)}</p>
+                   <p class="${rating >= 3 ? "text-black" : "text-red-500"}" ">${rating} <span class=" ${rating >= 3 ? "text-yellow-500" : "text-red-500"}"><i class="fa-solid fa-star"></i></span></p>
 
 
             `
